8ae4585e32ace1e1851003ff2cdb10d7d4c8dde0
Mahdyar Hasanpour <git@mahdyar.me>
2021-08-29 16:10:33 +0430

    fix: do `delete_option()` on uninstall

--- inc/Base/Deactivate.php
+++ inc/Base/Deactivate.php
@@ -11,12 +11,5 @@ class Deactivate
     public static function deactivate()
     {
         flush_rewrite_rules();
-
-        delete_option('telelog_api_key');
-        delete_option('telelog_chat_id');
-        delete_option('telelog_on_post_update');
-        delete_option('telelog_on_post_publish');
-        delete_option('telelog_on_post_comment');
-        delete_option('telelog_on_login_fail');
     }
 }
--- uninstall.php
+++ uninstall.php
@@ -8,3 +8,10 @@
 
 // If this file is called directly, abort!
 defined('WP_UNINSTALL_PLUGIN') or die('-1');
+
+delete_option('telelog_api_key');
+delete_option('telelog_chat_id');
+delete_option('telelog_on_post_update');
+delete_option('telelog_on_post_publish');
+delete_option('telelog_on_post_comment');
+delete_option('telelog_on_login_fail');
