59307833a71e26cdc5992cd46739ca7f88589595
Mahdyar Hasanpour <git@mahdyar.me>
2021-10-04 19:25:04 +0330

    feat: add more details to woocommerce order new

--- inc/Telegram/WooCommerce/WooCommerceOrderNew.php
+++ inc/Telegram/WooCommerce/WooCommerceOrderNew.php
@@ -24,11 +24,17 @@ class WooCommerceOrderNew extends Telegram
     }
     public function woocommerce_order_new($order_id)
     {
-        $ip = $this->get_client_ip();
         $order = wc_get_order( $order_id );
+        $ip = $order->customer_ip_address;
         $order_id  = $order->get_id();
+        
+        $site_url = get_site_url();
+        $order_url = $site_url . '/wp-admin/post.php?post=' . $order_id . '&action=edit';
 
-        $title = __('New WooCommerce order', 'telelog') . ": <code>$order_id</code>";
-        $this->alert($title, null, __FUNCTION__, null, $ip, null);
+        $author = $order->get_billing_first_name() . ' ' . $order->get_billing_last_name();
+        $email = $order->get_billing_email();
+
+        $title = __('New WooCommerce order', 'telelog') . ": <a href='$order_url'>$order_id</a>";
+        $this->alert($title, null, __FUNCTION__, $author, $ip, $email);
     }
 }
